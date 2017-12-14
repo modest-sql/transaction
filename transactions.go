8aa23256a7f89e31eb0fcec5500b0847cb099d4b
roberto-627 <roberto.melara@hotmail.com>
2017-12-14 02:52:38 -0600

    DONE

--- transactions.go
+++ transactions.go
@@ -1,11 +1,19 @@
 package transaction
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
 	"sync"
+	"time"
 
 	"github.com/modest-sql/common"
 	"github.com/rs/xid"
 )
+
+//execWaitGroup is used to make suretransactions run in order.
+var execWaitGroup sync.WaitGroup
 
 //Transaction States.
 const (
@@ -14,29 +22,58 @@ const (
 	Done
 )
 
-//transactionLock is the global transaction mutex
-var transactionLock sync.Mutex
+//transactionLock is the global transaction rwmutex.
+var transactionLock sync.RWMutex
+
+//transactionQueriesLock is the global transaction queries lock.
 var transactionQueriesLock sync.Mutex
 
-//TChannel is used to communicate with StartTransactionManager
+//excecutionBatchLock is the excecution batch lock.
+var excecutionBatchLock sync.Mutex
+
+//tChannel is used to communicate with StartTransactionManager
 var tChannel = make(chan Transaction)
 
-var tManager manager
+//tManager is the Transaction Manager global instance.
+var transactionManager manager
+
+var config TransactionConfiguration
+
+//TransactionConfiguration contains configuration options read from a json file.
+type TransactionConfiguration struct {
+	//commandsDelay determines the delay in seconds between the excecution of each command in a transaction.
+	CommandsDelay json.Number `json:"excecution_delay"`
+
+	//transactionThreads determines the ammount of transactions to be run concurrently.
+	TransactionThreads json.Number `json:"execution_batch_size"`
+}
+
+func init() {
+	jsonFile, _ := os.Open("config.json")
+
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	json.Unmarshal([]byte(byteValue), &config)
+}
 
 //Transaction instance definition.
 type Transaction struct {
 	TransactionID         xid.ID   `json:"Transaction_ID"`
 	TransactionQueries    []string `json:"TransactionQueries"`
 	CommandsInTransaction []common.Command
-	TransactionState      int `json:"Transaction_State"`
+	TransactionState      int    `json:"Transaction_State"`
+	CurrentComand         string `json:"Current_Command"`
 }
 
-//newTransaction creates an instance of Transaction.
-func newTransaction(commands []common.Command) Transaction {
+//NewTransaction creates an instance of Transaction.
+func NewTransaction(commands []common.Command) Transaction {
 	T := Transaction{
 		TransactionID:         xid.New(),
 		CommandsInTransaction: commands,
 		TransactionState:      Queued,
+		CurrentComand:         "None in excecution yet.",
 	}
 
 	for _, command := range commands {
@@ -48,15 +85,62 @@ func newTransaction(commands []common.Command) Transaction {
 
 //excecuteTransaction excectes the commands inside a transaction.
 func (T *Transaction) excecuteTransaction() {
-	transactionLock.Lock()
 	T.TransactionState = InProgress
 
-	for _, command := range T.CommandsInTransaction {
-		command.Instruction()
+	MutexesMap := new(sync.Map)
+
+	//ADD A RWMUTEX FOR EACH TABLE INVOLVED IN THIS TRANSACTION EXCECUTION QUEUE.
+	for i := 0; i < len(T.CommandsInTransaction); i++ {
+		MutexesMap.Store(T.CommandsInTransaction[i].TableName(), &sync.RWMutex{})
 	}
 
-	transactionLock.Unlock()
+	//INSERT LOCKS IN-BETWEEN COMMANDS.
+	XSLOCKEDTRANSACTION := make([]interface{}, 0)
+
+	for i := 0; i < len(T.CommandsInTransaction); i++ {
+		if T.CommandsInTransaction[i].InstructionType == 1 {
+			XSLOCKEDTRANSACTION = append(XSLOCKEDTRANSACTION, "RLOCK")
+			XSLOCKEDTRANSACTION = append(XSLOCKEDTRANSACTION, T.CommandsInTransaction[i])
+			XSLOCKEDTRANSACTION = append(XSLOCKEDTRANSACTION, "RUNLOCK")
+		} else {
+			XSLOCKEDTRANSACTION = append(XSLOCKEDTRANSACTION, "LOCK")
+			XSLOCKEDTRANSACTION = append(XSLOCKEDTRANSACTION, T.CommandsInTransaction[i])
+			XSLOCKEDTRANSACTION = append(XSLOCKEDTRANSACTION, "UNLOCK")
+		}
+	}
+
+	//EXCECUTE TRANSACTION EXCECUTION QUEUE.
+	for i := 0; i < len(XSLOCKEDTRANSACTION); i++ {
+		switch C := XSLOCKEDTRANSACTION[i].(type) {
+		case string:
+			if C == "RLOCK" {
+				Lock, _ := MutexesMap.Load((XSLOCKEDTRANSACTION[i+1].(common.Command)).TableName())
+				(Lock.(*sync.RWMutex)).RLock()
+			} else if C == "RUNLOCK" {
+				Lock, _ := MutexesMap.Load((XSLOCKEDTRANSACTION[i-1].(common.Command)).TableName())
+				(Lock.(*sync.RWMutex)).RUnlock()
+			} else if C == "LOCK" {
+				Lock, _ := MutexesMap.Load((XSLOCKEDTRANSACTION[i+1].(common.Command)).TableName())
+				(Lock.(*sync.RWMutex)).Lock()
+			} else if C == "UNLOCK" {
+				Lock, _ := MutexesMap.Load((XSLOCKEDTRANSACTION[i-1].(common.Command)).TableName())
+				(Lock.(*sync.RWMutex)).Unlock()
+			}
+		case common.Command:
+			T.CurrentComand = C.String()
+			C.Instruction()
+			Delay, _ := config.CommandsDelay.Int64()
+			time.Sleep(time.Second * time.Duration(Delay))
+			T.CurrentComand = "Waiting..."
+		default:
+			fmt.Println("Unknown")
+		}
+	}
+
 	T.TransactionState = Done
+	T.CurrentComand = "Transaction Finished."
+	execWaitGroup.Done()
+
 }
 
 //manager instance definition.
@@ -69,7 +153,7 @@ func GetTransactions() []Transaction {
 	Transactions := make([]Transaction, 0)
 
 	transactionQueriesLock.Lock()
-	for _, transaction := range tManager.TransactionQueue {
+	for _, transaction := range transactionManager.TransactionQueue {
 		Transactions = append(Transactions, transaction)
 	}
 	transactionQueriesLock.Unlock()
@@ -80,29 +164,182 @@ func GetTransactions() []Transaction {
 //popTransactionQueue pops the first transaction in the queue.
 func popTransactionQueue() {
 	transactionQueriesLock.Lock()
-	tManager.TransactionQueue = tManager.TransactionQueue[1:]
+	transactionManager.TransactionQueue = transactionManager.TransactionQueue[1:]
 	transactionQueriesLock.Unlock()
 }
 
 //addTransactionToQueue adds a new Transaction to the TransactionManager TransactionQueue.
 func addTransactionToQueue(t Transaction) {
 	transactionQueriesLock.Lock()
-	tManager.TransactionQueue = append(tManager.TransactionQueue, t)
+	transactionManager.TransactionQueue = append(transactionManager.TransactionQueue, t)
 	transactionQueriesLock.Unlock()
 }
 
 //AddCommands prepares the commands to be run in a transaction.
 func AddCommands(commands []common.Command) {
-	tChannel <- newTransaction(commands)
+	tChannel <- NewTransaction(commands)
+}
+
+func receiveTransactions() {
+	for {
+		transaction := <-tChannel
+		addTransactionToQueue(transaction)
+	}
 }
 
 /*StartTransactionManager is ment to be called once by the engine, afterwards it will receive incoming
 transactions and will add them to its queue and then will excecute them by moving it into the excecution
 batch.*/
 func StartTransactionManager() {
+	go receiveTransactions()
 	for {
-		transaction := <-tChannel
-		addTransactionToQueue(transaction)
-		go transaction.excecuteTransaction()
+		excecutionBatchLock.Lock()
+		executeBatch(len(transactionManager.TransactionQueue))
 	}
+}
+
+func executeBatch(actualBatchSize int) {
+	Threads, _ := config.TransactionThreads.Int64()
+	if len(transactionManager.TransactionQueue) > 0 {
+		if actualBatchSize < int(Threads) {
+			//DETERMINE TRANSACTION'S RELEVANCE.
+			TRANSACTIONDESIGNATION := make([]int, actualBatchSize)
+			for i := 0; i < actualBatchSize; i++ {
+				for j := 0; j < len(transactionManager.TransactionQueue[i].CommandsInTransaction); j++ {
+					if transactionManager.TransactionQueue[i].CommandsInTransaction[j].InstructionType == 0 {
+						if TRANSACTIONDESIGNATION[i] != 2 {
+							TRANSACTIONDESIGNATION[i] = 1
+						}
+					} else if transactionManager.TransactionQueue[i].CommandsInTransaction[j].InstructionType == 4 || transactionManager.TransactionQueue[i].CommandsInTransaction[j].InstructionType == 5 {
+						TRANSACTIONDESIGNATION[i] = 2
+					} else {
+						//Do nothing
+					}
+				}
+			}
+
+			//REORDER TRANSACTIONS IN A NEW ARRAY.
+			REORDEREDTRANSACTIONS := make([]Transaction, 0)
+
+			for i := 0; i < actualBatchSize; i++ {
+				if TRANSACTIONDESIGNATION[i] == 1 {
+					REORDEREDTRANSACTIONS = append(REORDEREDTRANSACTIONS, transactionManager.TransactionQueue[i])
+				}
+			}
+			for i := 0; i < actualBatchSize; i++ {
+				if TRANSACTIONDESIGNATION[i] == 0 {
+					REORDEREDTRANSACTIONS = append(REORDEREDTRANSACTIONS, transactionManager.TransactionQueue[i])
+				}
+			}
+
+			for i := 0; i < actualBatchSize; i++ {
+				if TRANSACTIONDESIGNATION[i] == 2 {
+					REORDEREDTRANSACTIONS = append(REORDEREDTRANSACTIONS, transactionManager.TransactionQueue[i])
+				}
+			}
+
+			//REPLACE REORDERED TRANSACTIONS IN TRANSACTION QUEUE.
+			for i := 0; i < actualBatchSize; i++ {
+				transactionManager.TransactionQueue[i] = REORDEREDTRANSACTIONS[i]
+			}
+
+			//MAKE SURE TRANSACTION RUNS COMMANDS IN ORDER.
+			execWaitGroup.Add(Contains(TRANSACTIONDESIGNATION, 1))
+			for i := 0; i < Contains(TRANSACTIONDESIGNATION, 1); i++ {
+				go transactionManager.TransactionQueue[i].excecuteTransaction()
+			}
+			execWaitGroup.Wait()
+			execWaitGroup.Add(Contains(TRANSACTIONDESIGNATION, 0))
+			for i := 0; i < Contains(TRANSACTIONDESIGNATION, 0); i++ {
+				go transactionManager.TransactionQueue[i].excecuteTransaction()
+			}
+			execWaitGroup.Wait()
+			execWaitGroup.Add(Contains(TRANSACTIONDESIGNATION, 2))
+			for i := 0; i < Contains(TRANSACTIONDESIGNATION, 2); i++ {
+				go transactionManager.TransactionQueue[i].excecuteTransaction()
+			}
+			execWaitGroup.Wait()
+
+			//POP DONE TRANSACTIONS FROM QUEUE.
+			for i := 0; i < actualBatchSize; i++ {
+				popTransactionQueue()
+			}
+
+		} else {
+			//DETERMINE TRANSACTION'S RELEVANCE.
+			TRANSACTIONDESIGNATION := make([]int, Threads)
+			for i := 0; i < int(Threads); i++ {
+				for j := 0; j < len(transactionManager.TransactionQueue[i].CommandsInTransaction); j++ {
+					if transactionManager.TransactionQueue[i].CommandsInTransaction[j].InstructionType == 0 {
+						if TRANSACTIONDESIGNATION[i] != 2 {
+							TRANSACTIONDESIGNATION[i] = 1
+						}
+					} else if transactionManager.TransactionQueue[i].CommandsInTransaction[j].InstructionType == 4 || transactionManager.TransactionQueue[i].CommandsInTransaction[j].InstructionType == 5 {
+						TRANSACTIONDESIGNATION[i] = 2
+					} else {
+						//Do nothing
+					}
+				}
+			}
+
+			//REORDER TRANSACTIONS IN A NEW ARRAY.
+			REORDEREDTRANSACTIONS := make([]Transaction, 0)
+
+			for i := 0; i < int(Threads); i++ {
+				if TRANSACTIONDESIGNATION[i] == 1 {
+					REORDEREDTRANSACTIONS = append(REORDEREDTRANSACTIONS, transactionManager.TransactionQueue[i])
+				}
+			}
+			for i := 0; i < int(Threads); i++ {
+				if TRANSACTIONDESIGNATION[i] == 0 {
+					REORDEREDTRANSACTIONS = append(REORDEREDTRANSACTIONS, transactionManager.TransactionQueue[i])
+				}
+			}
+
+			for i := 0; i < int(Threads); i++ {
+				if TRANSACTIONDESIGNATION[i] == 2 {
+					REORDEREDTRANSACTIONS = append(REORDEREDTRANSACTIONS, transactionManager.TransactionQueue[i])
+				}
+			}
+
+			//REPLACE REORDERED TRANSACTIONS IN TRANSACTION QUEUE.
+			for i := 0; i < int(Threads); i++ {
+				transactionManager.TransactionQueue[i] = REORDEREDTRANSACTIONS[i]
+			}
+
+			//MAKE SURE TRANSACTION RUNS COMMANDS IN ORDER.
+			execWaitGroup.Add(Contains(TRANSACTIONDESIGNATION, 1))
+			for i := 0; i < Contains(TRANSACTIONDESIGNATION, 1); i++ {
+				go transactionManager.TransactionQueue[i].excecuteTransaction()
+			}
+			execWaitGroup.Wait()
+			execWaitGroup.Add(Contains(TRANSACTIONDESIGNATION, 0))
+			for i := 0; i < Contains(TRANSACTIONDESIGNATION, 0); i++ {
+				go transactionManager.TransactionQueue[i].excecuteTransaction()
+			}
+			execWaitGroup.Wait()
+			execWaitGroup.Add(Contains(TRANSACTIONDESIGNATION, 2))
+			for i := 0; i < Contains(TRANSACTIONDESIGNATION, 2); i++ {
+				go transactionManager.TransactionQueue[i].excecuteTransaction()
+			}
+			execWaitGroup.Wait()
+
+			//POP DONE TRANSACTIONS FROM QUEUE.
+			for i := 0; i < int(Threads); i++ {
+				popTransactionQueue()
+			}
+		}
+	}
+	excecutionBatchLock.Unlock()
+}
+
+//Contains determines if an int array contains a certain value, returns the amount of time the value is found.
+func Contains(s []int, e int) int {
+	c := 0
+	for _, a := range s {
+		if a == e {
+			c++
+		}
+	}
+	return c
 }
